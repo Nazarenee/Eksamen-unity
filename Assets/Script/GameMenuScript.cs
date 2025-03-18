728f3563b6e19d832c427f408606a6a929eeeecf
DeZoete <143606680+DeZoete@users.noreply.github.com>
2025-03-18 16:52:25 +0100

    Start scene virker

---
+++ Assets/Script/GameMenuScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Play button clicked!");  // Check if this message appears in the Console
+        SceneManager.LoadScene("Scene1"); // Replace with your actual scene name
+    }
+}
