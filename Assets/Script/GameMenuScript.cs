4c542b6d71b302e0f48d45725b8b7eda1a146d45
DeZoete <143606680+DeZoete@users.noreply.github.com>
2025-03-19 11:11:52 +0100

    MenuOpgraderingIgen

--- Assets/Script/GameMenuScript.cs
+++ Assets/Script/GameMenuScript.cs
@@ -8,4 +8,9 @@ public class GameMenuScript : MonoBehaviour
         Debug.Log("Play button clicked!");  // Check if this message appears in the Console
         SceneManager.LoadScene("Scene1"); // Replace with your actual scene name
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
